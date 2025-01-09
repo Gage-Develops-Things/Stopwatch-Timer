adc7fc7442238dc297080e808da4100a9b8c690b
SlangTang <93gaget@gmail.com>
2025-01-09 12:36:58 -0600

    making a bunch of changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,55 +1,76 @@
-// Declare global variables
-let body = document.body;
-let startButton = document.getElementById("startButton"); 
-let resetButton = document.getElementById("resetButton"); 
-let timeDisplay = document.getElementById("timeDisplay");
-let lap = document.getElementById("lapButton");
-let isRunning = false;
-let timerInterval;
-let time = 0;
+const time = document.querySelector('.stopwatch')
+const mainButton = document.querySelector('#main-button')
+const clearButton = document.querySelector('#clear-button')
+const stopwatch = { elapsedTime: 0 }
 
-// Create functions
+mainButton.addEventListener('click', () => {
+  if (mainButton.innerHTML === 'Start') {
+    startStopwatch();
+    mainButton.innerHTML = 'Stop'
+  } else {
+    stopwatch.elapsedTime += Date.now() - stopwatch.startTime
+    clearInterval(stopwatch.intervalId)
+    mainButton.innerHTML = 'Start'
+  }
+})
+
+clearButton.addEventListener('click', () => {
+  stopwatch.elapsedTime = 0
+  stopwatch.startTime = Date.now()
+  displayTime(0, 0, 0, 0)
+})
+
+function startStopwatch() {
+  //reset start time
+  stopwatch.startTime = Date.now();
+  //run `setInterval()` and save id
+  stopwatch.intervalId = setInterval(() => {
+    //calculate elapsed time
+    const elapsedTime = Date.now() - stopwatch.startTime + stopwatch.elapsedTime
+    //calculate different time measurements based on elapsed time
+    const milliseconds = parseInt((elapsedTime%1000)/10)
+    const seconds = parseInt((elapsedTime/1000)%60)
+    const minutes = parseInt((elapsedTime/(1000*60))%60)
+    const hour = parseInt((elapsedTime/(1000*60*60))%24);
+    //display time
+    displayTime(hour, minutes, seconds, milliseconds)
+  }, 100);
+}
+
+function displayTime(hour, minutes, seconds, milliseconds) {
+  const leadZeroTime = [hour, minutes, seconds, milliseconds].map(time => time < 10 ? `0${time}` : time)
+  time.innerHTML = leadZeroTime.join(':')
+}
 
 
 
 
+// below Doesnt work
+// startButton.addEventListener("click", function() {
+// 	if (isRunning) {
+// 	clearInterval(timerInterval);
+// 	startButton.textContent = "Start";
+//  } else {
+// 	timerInterval = setInterval(function() {
+// 	time++;
+// 	timeDisplay.textContent = formatTime(time);
+//  	}, 1000);
+// 	startButton.textContent = "Stop";
+// 	}
+// 	isRunning = !isRunning;
+// });
+
+// BELOW DOESNT WORK.  WEIRD INBETWEEN SECONDS. TOO SLOW/FAST
+// let totalMiliSeconds = 0
+// startButton.addEventListener("click", function(){
+//   setInterval(() => {
+//     totalMiliSeconds += 1;
+//     timeDisplay.innerHTML = totalMiliSeconds;
+//   }, 1)
+
+//   })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Add processes and call functions
-toggler.addEventListener('change', () => {
-    if (toggler.checked) {
-        body.classList.remove('dark');
-        body.classList.add('unicorn');
-        localStorage.setItem('theme', 'unicorn'); // Save preference
-    } else {
-        body.classList.remove('unicorn');
-        body.classList.add('dark');
-        localStorage.setItem('theme', 'dark'); // Save preference
-    }
-});
-
-startButton.addEventListener("click", function() {
-	if (isRunning) {
-	clearInterval(timerInterval);
-	startButton.textContent = "Start";
- } else {
-	timerInterval = setInterval(function() {
-	time++;
-	timeDisplay.textContent = formatTime(time);
- 	}, 1000);
-	startButton.textContent = "Stop";
-	}
-	isRunning = !isRunning;
+resetButton.addEventListener("click", function(){
+timeDisplay.textContent = "00.00.00.00";
 });
